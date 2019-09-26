3542a3ceaefa6e9c9cbfc413cad8b75f0e0fd0bb
James Bell <james@stellentus.com>
2019-09-26 10:21:25 -0600

    Create initial AppleScript

---
+++ platform/macOS/insertResources.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ../../open-field/Open_field_v2.py Rat\ Apps.app/Contents/Resources/python/
+cp ../../spg-analysis/SPG\ analysis\ v0.3.py Rat\ Apps.app/Contents/Resources/python/
+cp ../../white-box/white_box.py Rat\ Apps.app/Contents/Resources/python/
+cp ../../x-maze/X\ maze.py Rat\ Apps.app/Contents/Resources/python/
